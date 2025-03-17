e7188f886a3a405f9c7364c026e45d1ae57dd7a0
Max Chen <maxchen@maxs-mbp.mynetworksettings.com>
2025-03-17 11:59:44 -0400

    add service layer

--- src/main/java/com/java/interview/controller/HelloController.java
+++ src/main/java/com/java/interview/controller/HelloController.java
@@ -1,12 +1,19 @@
 package com.java.interview.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.java.interview.service.HelloService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private HelloService service;
     
     @GetMapping("hello/{name}")
     public String greetings(@PathVariable String name) {
---
+++ src/main/java/com/java/interview/service/HelloService.java
@@ -0,0 +1,8 @@
+package com.java.interview.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    
+}
